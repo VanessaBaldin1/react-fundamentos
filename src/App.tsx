3382c317251e81ceb34a46b81ec62c0329fbff2d
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-23 10:36:17 -0300

    Adiciona os elementos ao componente

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,51 @@ export default function App() {
         </nav>
       </header>
 
-      <main></main>
+      {/* Comentários no meio do JSX: é normal ficar {} */}
+      {/* Ao usar outros elementos HTML no JSX, é importante envolver tudo em um único <> </> (Fragmento React) */}
+      <main>
+        <section>
+          <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+          <p>Este é um exemplo de aplicação React.</p>
+
+          <article>
+            <h3>Artigo 1 de Exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
+              quidem molestiae consequatur, natus maiores tenetur hic veniam
+              recusandae autem magnam.
+            </p>
+          </article>
+
+          <article>
+            <h3>Artigo 2 de Exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
+              quidem molestiae consequatur, natus maiores tenetur hic veniam
+              recusandae autem magnam.
+            </p>
+          </article>
+
+          <article>
+            <h3>Artigo 3 de Exemplo</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+              aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
+              quidem molestiae consequatur, natus maiores tenetur hic veniam
+              recusandae autem magnam.
+            </p>
+          </article>
+        </section>
+      </main>
+
+      <footer>
+        <h2>Pequeno site criado com React</h2>
+        <p>
+          Desenvolvido por <b>Vanessa</b> &copy;2025{" "}
+        </p>
+      </footer>
     </>
   );
 }
