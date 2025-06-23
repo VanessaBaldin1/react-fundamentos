bbbd844d9ccf6e63a05c531d3174b59ec8e56c0b
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-23 10:03:02 -0300

    Adiciona uso de fragmento <> </> no html

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ export default function App() {
   // Comentário de uma linha
   /* sfit+ alt +A */
 
+  //A função só retorna um resultado, por isso precisa ser envolvido no div
+  // o pessoal da React criou <> </> mais usada chama de (FRAGMENTO)
   return (
-    <header>
-      <h1>Olá React! 😊😊😊</h1>
-      <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Sobre</a>
-        <a href="">Contato</a>
-      </nav>
-    </header>
+    <>
+      <header>
+        <h1>Olá React! 😊😊😊</h1>
+        <hr />
+        <nav>
+          <a href="">Home</a>
+          <a href="">Sobre</a>
+          <a href="">Contato</a>
+        </nav>
+      </header>
+
+      <main></main>
+    </>
   );
 }
