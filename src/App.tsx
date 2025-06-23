5f8e892fbb8b4f5eb28ce21b579592da3e5e2e8b
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-23 11:34:32 -0300

    finalizando o exercicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,15 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
+import Rodape from "./Rodape";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
-      <main>
-        <section>
-          <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
-          <p>Este é um exemplo de aplicação React.</p>
 
-          <article>
-            <h3>Artigo 1 de Exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-              aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
-              quidem molestiae consequatur, natus maiores tenetur hic veniam
-              recusandae autem magnam.
-            </p>
-          </article>
+      <Conteudo />
 
-          <article>
-            <h3>Artigo 2 de Exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-              aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
-              quidem molestiae consequatur, natus maiores tenetur hic veniam
-              recusandae autem magnam.
-            </p>
-          </article>
-
-          <article>
-            <h3>Artigo 3 de Exemplo</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
-              aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
-              quidem molestiae consequatur, natus maiores tenetur hic veniam
-              recusandae autem magnam.
-            </p>
-          </article>
-        </section>
-      </main>
-
-      <footer>
-        <h2>Pequeno site criado com React</h2>
-        <p>
-          Desenvolvido por <b>Vanessa</b> &copy;2025{" "}
-        </p>
-      </footer>
+      <Rodape />
     </>
   );
 }
