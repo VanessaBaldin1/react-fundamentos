31fed0986d90361351f376592fd1fb01e77804f0
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-23 09:35:44 -0300

    Comentários no main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Ajuda detectar uso de recursos obsoleto (APIS, funções, padrões etc)
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')!).render(
+// Usado para criar a 'raíz' da aplicação REACT a partir de um elemento HTML
+import { createRoot } from "react-dom/client";
+
+//Importa o componente principal da aplicação
+// O componente APP é o ponto de entrada da aplicação React
+import App from "./App.tsx";
+
+// Seleciona o elemento HTML root, cria a raizda aplicação e renderiza o componente APP
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
