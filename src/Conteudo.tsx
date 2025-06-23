c4979f9294a95973f5adb75a90dca02014be0bfc
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-23 11:28:35 -0300

    finalizando o exercicio

---
+++ src/Conteudo.tsx
@@ -0,0 +1,40 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+        <p>Este é um exemplo de aplicação React.</p>
+
+        <article>
+          <h3>Artigo 1 de Exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
+            quidem molestiae consequatur, natus maiores tenetur hic veniam
+            recusandae autem magnam.
+          </p>
+        </article>
+
+        <article>
+          <h3>Artigo 2 de Exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
+            quidem molestiae consequatur, natus maiores tenetur hic veniam
+            recusandae autem magnam.
+          </p>
+        </article>
+
+        <article>
+          <h3>Artigo 3 de Exemplo</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            aperiam quo explicabo error dolore iste dignissimos sed modi ipsum
+            quidem molestiae consequatur, natus maiores tenetur hic veniam
+            recusandae autem magnam.
+          </p>
+        </article>
+      </section>
+    </main>
+  );
+}
