1bf1d78731b133526a2ae57f394604a9550329a9
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-24 09:28:34 -0300

    Importando folha de estilo CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-// Ajuda detectar uso de recursos obsoleto (APIS, funções, padrões etc)
 import { StrictMode } from "react";
-
-// Usado para criar a 'raíz' da aplicação REACT a partir de um elemento HTML
 import { createRoot } from "react-dom/client";
-
-//Importa o componente principal da aplicação
-// O componente APP é o ponto de entrada da aplicação React
 import App from "./App.tsx";
 
-// Seleciona o elemento HTML root, cria a raizda aplicação e renderiza o componente APP
+//Importando a folha de estilos CSS global
+import "./global.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
