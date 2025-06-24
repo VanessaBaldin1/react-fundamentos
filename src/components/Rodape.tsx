09d2876d5aa0e3012026da7c9afa383d09c65056
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-24 10:03:38 -0300

    Criando objeto com CSS

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,7 +1,17 @@
+import type { CSSProperties } from "react";
+
+const estilosRodape: CSSProperties = {
+  backgroundColor: "#222",
+  color: "white",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
     <>
-      <footer>
+      {/* o Style nesse caso é aplicado somente o estiloRodape já é um objeto */}
+      <footer style={estilosRodape}>
         <h2>Pequeno site criado com React</h2>
         {/* CSS IN JS =  o Style o segundo { é um objeto} */}
         <p style={{ color: "red", fontWeight: "bold" }}>
