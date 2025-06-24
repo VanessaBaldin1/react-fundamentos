3c1d9a6c1dda0353ce6427024cf9d7006ac0e365
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-24 11:00:39 -0300

    Cria e aplica modulo CSS ao Cabecalho

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -2,10 +2,14 @@
 import logo from "../assets/logo-react.png";
 import Menu from "./Menu";
 
+// Importando o módulo CSS para o componente Cabecalho (styles é a convensão pode usar qualquer nome)
+import estilos from "./Cabecalho.module.css";
+
 //para usar a imagem tira as "" do src e coloca {} com a informação logo
+// classeName={ estilos [.topo-site]} - quando usado com - no nome da classe como .topo-site
 export default function Cabecalho() {
   return (
-    <header>
+    <header className={estilos.topoSite}>
       <h1>
         <img src={logo} alt="" /> Olá React! 😊😊😊
       </h1>
