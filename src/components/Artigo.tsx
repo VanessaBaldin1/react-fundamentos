7a1002a5410b8fd6ae8ff6fe0d186b09ee74e453
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-24 09:13:08 -0300

    Cria componente Artigo

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,13 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de Exemplo</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aperiam
+        quo explicabo error dolore iste dignissimos sed modi ipsum quidem
+        molestiae consequatur, natus maiores tenetur hic veniam recusandae autem
+        magnam.
+      </p>
+    </article>
+  );
+}
