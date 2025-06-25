b902dc89f58e9a7ac348553dc5610a159c9c7071
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-25 08:42:31 -0300

    atualiza o menu

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -5,7 +5,7 @@ import Menu from "./Menu";
 // Importando o módulo CSS para o componente Cabecalho (styles é a convensão pode usar qualquer nome)
 import estilos from "./Cabecalho.module.css";
 
-//para usar a imagem tira as "" do src e coloca {} com a informação logo
+//para usar a imagem tira as "" do src e coloca {} com a informação logo, por que fez a importação
 // classeName={ estilos [.topo-site]} - quando usado com - no nome da classe como .topo-site
 export default function Cabecalho() {
   return (
