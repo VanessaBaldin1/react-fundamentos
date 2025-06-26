8d323d8b37278b400cd8d527bb3d22c1b9b9d1e8
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-26 08:57:54 -0300

    Usa mas props e template string

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -11,7 +11,9 @@ export default function Conteudo() {
       <section
         className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
       >
-        <Saudacao nome="Vanessa" />
+        <h2 className="font-bold text-lg">Fundamentos de React!</h2>
+        <Saudacao nome="Vanessa" classe="bg-emerald-100" />
+        <Saudacao nome="Luis Miguel" classe="bg-blue-200" />
 
         <p>Este é um exemplo de aplicação React.</p>
 
