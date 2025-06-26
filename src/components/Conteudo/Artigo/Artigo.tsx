18226a5af334d2102571c8bb41774ee724c471d6
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-26 09:58:57 -0300

    aplica renderização condicional

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -5,16 +5,30 @@ type ArtigoProps = {
   titulo: string;
   numero: number;
   children: ReactNode;
+  data?: Date;
 };
 
 // Deixando o conteudo dinâmico as chamadas/aplicações são direto com { }
 
-export default function Artigo({ titulo, numero, children }: ArtigoProps) {
+export default function Artigo({
+  titulo,
+  numero,
+  children,
+  data,
+}: ArtigoProps) {
   return (
     <article className={estilos.artigo}>
       <h3>
         Artigo {numero}: {titulo}
       </h3>
+
+      {/* RENDERIZAÇÃO CONDICIONAL: só mostra o parágrafo se existir um valor na prop data */}
+      {data && ( //o && neste caso vira um IF - RENDERIZAÇÃO CONDICIONAL
+        <p>
+          Data do registro: <time>{data?.toLocaleDateString()}</time>
+        </p>
+      )}
+
       {children}
     </article>
   );
