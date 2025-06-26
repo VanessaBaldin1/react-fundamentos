ceeb67ed0e0cfc4e83450608af86bfd2d266b53f
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-26 09:30:31 -0300

    Aplica children no artigo

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,20 +1,21 @@
+import type { ReactNode } from "react";
 import estilos from "./Artigo.module.css";
 
 type ArtigoProps = {
   titulo: string;
-  texto: string;
   numero: number;
+  children: ReactNode;
 };
 
 // Deixando o conteudo dinâmico as chamadas/aplicações são direto com { }
 
-export default function Artigo({ titulo, texto, numero }: ArtigoProps) {
+export default function Artigo({ titulo, numero, children }: ArtigoProps) {
   return (
     <article className={estilos.artigo}>
       <h3>
         Artigo {numero}: {titulo}
       </h3>
-      <p>{texto}</p>
+      {children}
     </article>
   );
 }
