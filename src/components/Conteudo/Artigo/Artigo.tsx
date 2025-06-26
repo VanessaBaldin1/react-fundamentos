aa19ea18129ad61163dd7e9620770d09e02bab1e
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-26 10:50:43 -0300

    Ajusta componentes para novos dados

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,35 +1,25 @@
-import type { ReactNode } from "react";
 import estilos from "./Artigo.module.css";
+import type { Curso } from "../../../types/Curso";
 
 type ArtigoProps = {
-  titulo: string;
-  numero: number;
-  children: ReactNode;
-  data?: Date;
+  dados: Curso;
 };
 
 // Deixando o conteudo dinâmico as chamadas/aplicações são direto com { }
 
-export default function Artigo({
-  titulo,
-  numero,
-  children,
-  data,
-}: ArtigoProps) {
+export default function Artigo({ dados }: ArtigoProps) {
+  const { titulo, preco, categoria } = dados;
   return (
     <article className={estilos.artigo}>
-      <h3>
-        Artigo {numero}: {titulo}
-      </h3>
-
-      {/* RENDERIZAÇÃO CONDICIONAL: só mostra o parágrafo se existir um valor na prop data */}
-      {data && ( //o && neste caso vira um IF - RENDERIZAÇÃO CONDICIONAL
-        <p>
-          Data do registro: <time>{data?.toLocaleDateString()}</time>
-        </p>
-      )}
-
-      {children}
+      <h3> {titulo}</h3>
+      <p>
+        <b>
+          Categoria: <b>{categoria}</b>
+        </b>
+      </p>
+      <p>
+        Preço: <b>{preco}</b>
+      </p>
     </article>
   );
 }
