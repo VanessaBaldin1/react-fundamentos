e74be9452fc3071ffde422c6902a2b65044e8c14
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-27 09:36:25 -0300

    Aplica useState no cabecalho titulo

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -4,14 +4,23 @@ import Menu from "./Menu/Menu";
 
 // Importando o módulo CSS para o componente Cabecalho (styles é a convensão pode usar qualquer nome)
 import estilos from "./Cabecalho.module.css";
+import { useState } from "react";
 
 //para usar a imagem tira as "" do src e coloca {} com a informação logo, por que fez a importação
 // classeName={ estilos [.topo-site]} - quando usado com - no nome da classe como .topo-site
+
 export default function Cabecalho() {
+  // Aqui definimos a chamada de uma função especial do React = ESTUDAR HOOKS
+
+  // useState. Esta e outras funções iniciadas com o prefixo 'use' são conhecidas como React HOOKS
+
+  //no Caso, do useState, usando desestruturação, criamos uma constate que representa o valor do state (titulo) e uma função responsável por atualizar o state (setTitulo)
+  const [titulo, setTitulo] = useState("Olá React!");
+
   return (
     <header className={estilos.topoSite}>
       <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
-        <img className={estilos.logo} src={logo} alt="" /> Olá React! 😊😊😊
+        <img className={estilos.logo} src={logo} alt="" /> {titulo} 😊😊😊
       </h1>
       <hr />
 
