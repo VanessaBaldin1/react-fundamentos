b8611dcce2303da9019fd969a0bd4775dfc69d25
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-24 09:53:07 -0300

    Cria pasta pages e componentes Home, Sobre e Contato

---
+++ src/pages/Sobre.tsx
@@ -0,0 +1,13 @@
+export default function Sobre() {
+  return (
+    <section>
+      <h2>Sobre nossa empresa</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        debitis architecto harum perferendis illo vel a corporis distinctio quod
+        optio repudiandae atque facilis veniam doloribus corrupti ut reiciendis,
+        voluptate ipsa?
+      </p>
+    </section>
+  );
+}
