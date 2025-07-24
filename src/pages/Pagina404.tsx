59682b98d30d0ae8cf4764b1a9f10abcfeebd484
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-24 11:41:23 -0300

    Ajuste de layout na pagina 404

--- src/pages/Pagina404.tsx
+++ src/pages/Pagina404.tsx
@@ -4,20 +4,21 @@ export default function Pagina404() {
   return (
     <section>
       <h1>💥 Opa! Você entrou em um universo paralelo...</h1>
+      <br />
       <p>
         A página que você está procurando se perdeu no espaço-tempo da internet.
         Mas não se preocupe até os melhores exploradores se desviam da rota às
         vezes. 🚀
       </p>
-
+      <br />
       <p>
         Não é o destino, mas a jornada que nos transforma. Desconhecido (ou
         talvez um programador cansado 👨‍💻)
       </p>
 
+      <br />
       <p>
         <Link to="/">🔁 Voltar ao início</Link>
-        🔍 Ou continue sua jornada pelo nosso site e descubra novos mundos!
       </p>
     </section>
   );
