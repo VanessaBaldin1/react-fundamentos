e00b4757a5ce09dd276d5cb72be6c34015438bd2
Vanessa Baldin <vanbaldin08@gmail.com>
2025-07-25 09:42:16 -0300

    Adiciona useEffect nas paginas
    Inclui Home, Sobre, Contato e pagina 404

--- src/pages/Pagina404.tsx
+++ src/pages/Pagina404.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Pagina404() {
+  useEffect(() => {
+    document.title = " ERRO 404 | Meu app React";
+  });
+
   return (
     <section>
       <h1>💥 Opa! Você entrou em um universo paralelo...</h1>
